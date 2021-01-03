2e78bb1cf7dbae7709e50ff5b0628e0cee84fd06
Ranveer Raghuwanshi <ranveer.raghuwanshi@wolfpack-dcs.com>
2021-01-03 18:54:57 +0100

    indentation

--- background.js
+++ background.js
@@ -1,15 +1,18 @@
 chrome.webRequest.onBeforeRequest.addListener(
-  function(info) {
-    if (info.url && info.url.indexOf('ViewImpexMonitor-DownloadLogFile') != -1) {
-      chrome.tabs.sendMessage(info.tabId, info, {frameId: info.frameId});
+  function (info) {
+    if (
+      info.url &&
+      info.url.indexOf("ViewImpexMonitor-DownloadLogFile") != -1
+    ) {
+      chrome.tabs.sendMessage(info.tabId, info, { frameId: info.frameId });
       return {
-        redirectUrl:"javascript:"
-      }
+        redirectUrl: "javascript:",
+      };
     }
   },
   {
     urls: ["*://*.demandware.net/on/demandware.store/Sites-Site/default/*"],
-    types: ["main_frame"]
+    types: ["main_frame"],
   },
   ["blocking"]
 );
--- jobLogViewer.js
+++ jobLogViewer.js
@@ -1,25 +1,24 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      var xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function() {
-          if (this.readyState == 4) {
-              console.log(xhr.responseText);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      console.log(xhr.responseText);
 
-              var head = document.head;
-              // var css = 'pre { display: block; word-wrap: break-word; white-space: pre-wrap; overflow-x: auto; }';
-              var css = 'pre { 	box-sizing: border-box; width: 100%; white-space: pre-wrap; padding: 0; margin: 0; overflow: auto; overflow-y: hidden; font-size: 12px; line-height: 20px; background: #efefef; border: 1px solid #777; padding: 10px; color: #333; }';
-              var style = document.createElement('style');
-              head.appendChild(style);
-              style.type = 'text/css';
-              style.appendChild(document.createTextNode(css));
+      var head = document.head;
+      // var css = 'pre { display: block; word-wrap: break-word; white-space: pre-wrap; overflow-x: auto; }';
+      var css =
+        "pre { 	box-sizing: border-box; width: 100%; white-space: pre-wrap; padding: 0; margin: 0; overflow: auto; overflow-y: hidden; font-size: 12px; line-height: 20px; background: #efefef; border: 1px solid #777; padding: 10px; color: #333; }";
+      var style = document.createElement("style");
+      head.appendChild(style);
+      style.type = "text/css";
+      style.appendChild(document.createTextNode(css));
 
-              const dialog = document.createElement('dialog');
-              dialog.innerHTML = '<pre>' + xhr.responseText + '</pre>';
-              document.body.appendChild(dialog);
-              dialog.showModal();
-          }
-      };
-      xhr.open("GET", request.url);
-      xhr.send();
+      const dialog = document.createElement("dialog");
+      dialog.innerHTML = "<pre>" + xhr.responseText + "</pre>";
+      document.body.appendChild(dialog);
+      dialog.showModal();
     }
-);
+  };
+  xhr.open("GET", request.url);
+  xhr.send();
+});
